e95aa55e548fba13d66ab114b2b285ba6c65e415
Gus Vargas <gusvargas@gmail.com>
2025-01-07 13:29:22 -0600

    bump

--- Formula/metaplane.rb
+++ Formula/metaplane.rb
@@ -1,11 +1,11 @@
 class Metaplane < Formula
   desc "Metaplane CLI"
   homepage "https://github.com/metaplane/cli"
-  version "0.1.0"
+  version "0.1.1"
   depends_on arch: :arm64
   
   url "https://cli.metaplane.dev/spm/download/macos-arm64/metaplane", using: :nounzip
-  sha256 "8bdb8862d8f4594788e6bd08d2d48d3b193f761ff2b0113afec0909a97f62e78"
+  sha256 "7d8fd363a0ec8eef75a40c5918badbfa717aa2f5b6c9ce42ad3391b434101423"
 
   def install
     bin.install "metaplane"
